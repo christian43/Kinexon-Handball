5eaa61d9492bb89db8053cd6459de18477918c7f
christian saal <christiansaal@CSaal-MBP.local>
2022-03-10 16:51:20 +0100

    add count games

--- summary_export_match.R
+++ summary_export_match.R
@@ -1,17 +1,24 @@
 #### Packages ####
 library(summarytools)
 
+#### read data ####
+dat <- fread("Data/export_match.csv", header = TRUE)
+
 #### allgemeines ####
 
 # Wieviel Spieler
-length(unique(dat_match$Name))
-unique(dat_match$Name)
+length(unique(dat$Name))
+unique(dat$Name)
 # setdiff( unique(dat_match$Name), unique(dat_HZ$Name)) # Dominik Eckart in
 # match but not in HZ 1 or 2
 
 # Wieviel Spiele
-length(unique(dat_match$`Session begin date (Local timezone)`))
-length(unique(dat_match$`Session ID`))
+length(unique(dat$`Session begin date (Local timezone)`))
+length(unique(dat$`Session ID`))
 # check Description
 table(dat_match$Description)
-
+length(unique(dat$Description))
+# Wieviele Saisons
+unique(dat$Saison)
+# Wieviele Spieltage
+dat[,length(unique(Spieltag)), by = Saison]
