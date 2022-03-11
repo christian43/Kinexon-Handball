6c068c4883c160276028ec93a927bf6adb7a032e
christian saal <christiansaal@CSaalMBP.fritz.box>
2022-03-11 17:31:05 +0100

    put BHC and DHFK together

--- get_data.R
+++ get_data.R
@@ -1,9 +1,16 @@
+#### set Path ####
+setwd(Sys.getenv("MY_Ext"))
+
 #### load packages
 
 #### load event*.csv files #####
 
+# set Verein und Pfad
+Verein <- "DHFK"
+Path <- paste0("RAW/",Verein)
 
-all.files <- list.files(path = "data/", 
+# load all files 
+all.files <- list.files(path = Path, 
   recursive = TRUE,
   pattern = "Event",
   full.names = TRUE)
@@ -12,15 +19,21 @@ l <- lapply(all.files, fread, sep=";", fill = TRUE, na.strings="")
 
 #### remove colnames and format local timestamp ####
 
-source("clean_event.R")
+# source("clean_event.R")
 l <- lapply(l, function(x) clean_event(x))
-event <- rbindlist( l, fill = TRUE)
+names(l) <- all.files
+event <- rbindlist( l, fill = TRUE, idcol = "File")
 event <- event[,dateS := as.POSIXct(format(event$'Timestamp in local format',
   format = "%Y-%m-%d"))]
 
+### add filename and split filename to get Saison and Spieltag ### 
+cols <- c("RAW","Verein","Saison", "Spieltag1","Spieltag","Filename")
+event[, c(cols) := tstrsplit(File, "/", fixed=TRUE)]
+event[ ,c("File", "RAW", "Spieltag1","Filename") := NULL]
+
 #### read and prepare data from statistic*.csv files ####
 
-all.files <- list.files(path = "data", 
+all.files <- list.files(path = Path, 
   recursive = TRUE,
   pattern = "export",
   full.names = TRUE)
@@ -61,8 +74,8 @@ lapply(event.list, function(x) remove_NA(x))
 
 #### load colnames for event data and rename colnames in event list ####
 
-load("Rkinexon/data/kinexonEventNames.rda")
-kinexonEventNames <- lapply(kinexonEventNames, function(x) {x <-  c("dateS",x, "club", "ids")})
+load("~/Documents/Projekte/Ul/Handball/Kinexon/R/Rkinexon/data/kinexonEventNames.rda")
+kinexonEventNames <- lapply(kinexonEventNames, function(x) {x <-  c("dateS",x,"Verein", "Saison", "Spieltag", "club", "ids")})
 
 colnames(event.list$'Acceleration')             <- kinexonEventNames$'Accelerations'
 colnames(event.list$'Ball Possession Lost')     <- kinexonEventNames$'Ball Possession (lost)'
@@ -79,12 +92,16 @@ colnames(event.list$'Sprint')                   <- kinexonEventNames$'Sprints'
 
 #### remove all except list of data frames
 
-rm(list=setdiff(ls(), "event.list"))
+rm(list=setdiff(ls(), c("event.list","event.list.BHC", "event.list.DHFK")))
 
 #### stack club data ####
 # ToDo combine data.frame BHC and DJFK
 
+event.list.DHFK <- event.list
+event.list.BHC <- event.list
 
+event.list.DHFK <- event.list.DHFK[order(names(event.list.DHFK))]
+event.list.BHC <- event.list.BHC[order(names(event.list.BHC))]
 
-
+final.list <- Map(rbind,event.list.DHFK, event.list.BHC)
 
