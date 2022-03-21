5a09ed451199e40ee9861432aa58a633db8e5fc2
christian saal <christiansaal@CSaalMBP.fritz.box>
2022-03-21 09:23:24 +0100

    summary by halftime

--- summary_export_match.R
+++ summary_export_match.R
@@ -1,11 +1,13 @@
 #### Packages ####
-library(papeR)
+library(NbClust)
+library(pheatmap)
+library(RColorBrewer)
+library(summarytools)
+library(corrplot)
 
 #### set Path ####
 setwd(Sys.getenv("kinexonPath"))
 
-#### Packages ####
-library(summarytools)
 
 #### read data ####
 dat <- fread("Data/CSV/export_match.csv", header = TRUE, stringsAsFactors=TRUE)
@@ -32,15 +34,15 @@ unique(dat$Saison)
 dat[,length(unique(Spieltag)), by = list(Verein,Saison)]
 
 # Time on Playing field #
-
+playtime <- 600
 summary(dat$`Time on Playing Field (hh:mm:ss)`)
 colSums(dat == 0)[37]
-dat <- dat[`Time on Playing Field (hh:mm:ss)` != 0]
+dat <- dat[!`Time on Playing Field (hh:mm:ss)` < playtime]
 
 
 # Distance #
 summary(dat$`Distance (m)`)
-colSums(dat == 0)[13]
+colSums(dat ==  0)[13]
 dat <- dat[`Distance (m)` != 0]
 
 # print summary
@@ -57,6 +59,7 @@ print(s, method = 'browser')
 # Impacts
 # shots
 # passes
+# Speed
 
 cols <- c('Session begin date (Local timezone)', 
   'Session ID',
@@ -75,7 +78,8 @@ cols <- c('Session begin date (Local timezone)',
   'Impacts',
   'Jumps',
   'Passes',
-  'Shots'
+  'Shots',
+  'Speed (max.) (km/h)'
 )
 datlast <- dat[,..cols]
 
@@ -101,7 +105,103 @@ ggplot(datlong[!Position=='TW'], aes(y = value, x = Position), fill = Position) 
 
 x <- datlong[,.(mean = round(mean(value, na.rm=TRUE),2),
   sd = round(sd(value, na.rm=TRUE))), by = list(variable, Position)][order(variable, mean)]
-
 x[, new := paste0(mean," (", sd,")", sep = " ")]
 dcast(x, Position ~ variable , value.var = list("new"))
+
+#### correlation between vars ####
+
+nums <- dat[, lapply(dat, is.numeric) == TRUE, with = FALSE]
+nums <- nums[,!c('Spieltag','V1', 'Group Id', 'League ID','Player ID', 'Session ID', 'Number' )]
+
+x <- colSums(is.na(nums)) 
+cols <- names(x[x > 100])
+nums <- nums[,!..cols]
+nums <- nums[complete.cases(nums),]
+
+x <- colSums(nums == 0)
+cols <- names(x[x > 400])
+nums <- nums[,!..cols]
+
+perfscaled <- data.frame(scale(nums))
+
+nc <- NbClust(nums,
+  distance="maximum",
+  min.nc=2,
+  max.nc=5,
+  method="ward.D2")
+
+pheatmap(nums, cluster_rows=FALSE )
+
+cormat <- cor(nums)
+corrplot(cormat, order = "hclust",
+  hclust.method = 'complete', 
+  tl.cex = 0.5, 
+  method = "square")
+
+# corrplot(cormat, order = "FPC", tl.cex = 0.3, addrect = 3)
+
+#### 1 und 2 HZ ####
+
+dat <- fread("Data/CSV/export_HZ.csv", header = TRUE, stringsAsFactors=TRUE)
+
+# Time on Playing field #
+playtime <- 20
+summary(dat$`Time on Playing Field (hh:mm:ss)`)
+colSums(dat == 0)[37]
+dat <- dat[!`Time on Playing Field (hh:mm:ss)` < playtime]
+
+
+# Distance #
+summary(dat$`Distance (m)`)
+colSums(dat ==  0)[13]
+dat <- dat[`Distance (m)` != 0]
+
+cols <- c('Session begin date (Local timezone)', 
+  'Session ID',
+  'Description',
+  'Verein',
+  'Saison',
+  'Name',
+  'Position',
+  'Time on Playing Field (hh:mm:ss)',
+  'Distance (m)',
+  'Distance (speed | Very high) (m)',
+  'Distance (speed | High) (m)',
+  'Distance (speed | Low) (m)',
+  'Distance (speed | Medium) (m)',
+  'Distance (speed | Very low) (m)',
+  'Changes of Direction',
+  'Impacts',
+  'Jumps',
+  'Passes',
+  'Shots',
+  'Speed (max.) (km/h)'
+)
+datlast <- dat[,..cols]
+
+# Wieviel Spieler
+length(unique(datlast$Name))
+
+# Wieviel Spiele
+length(unique(datlast$`Session ID`))
+
+# Wieviele Saisons
+unique(datlast$Saison)
+
+summary(datlast)
+# show rows with NA
+datlast[!complete.cases(datlast), ]
+
+# convert to long format
+ids <- names(datlast)[1:7]
+datlong <- melt(datlast, id.vars = ids)
+
+# simple check 
+ggplot(datlong[!Position=='TW'], aes(y = value, x = Description)) + geom_boxplot() + facet_wrap(~variable, scale="free")
+ggplot(datlong[!Position=='TW'], aes(y = value, x = Description, fill = Position)) + geom_boxplot() + facet_wrap(~variable, scale="free")
+
+x <- datlong[,.(mean = round(mean(value, na.rm=TRUE),2),
+  sd = round(sd(value, na.rm=TRUE))), by = list(variable, Description)][order(variable, mean)]
+x[, new := paste0(mean," (", sd,")", sep = " ")]
+dcast(x, Description ~ variable , value.var = list("new"))
 
